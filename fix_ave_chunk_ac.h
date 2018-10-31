9cdd4bb6a6a0499000658a4c888a92608258c0b0
ramisetti <s.b.ramisetti@leeds.ac.uk>
2018-10-31 14:56:10 +0000

    renamed fix_ave_chunk_ac class

--- fix_ave_chunk_ac.h
+++ fix_ave_chunk_ac.h
@@ -13,22 +13,22 @@
 
 #ifdef FIX_CLASS
 
-FixStyle(ave/chunk,FixAveChunk)
+FixStyle(ave/chunk/ac,FixAveChunkAC)
 
 #else
 
-#ifndef LMP_FIX_AVE_CHUNK_H
-#define LMP_FIX_AVE_CHUNK_H
+#ifndef LMP_FIX_AVE_CHUNK_AC_H
+#define LMP_FIX_AVE_CHUNK_AC_H
 
 #include <stdio.h>
 #include "fix.h"
 
 namespace LAMMPS_NS {
 
-class FixAveChunk : public Fix {
+class FixAveChunkAC : public Fix {
  public:
-  FixAveChunk(class LAMMPS *, int, char **);
-  ~FixAveChunk();
+  FixAveChunkAC(class LAMMPS *, int, char **);
+  ~FixAveChunkAC();
   int setmask();
   void init();
   void setup(int);
@@ -92,11 +92,11 @@ Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
 
-E: No values in fix ave/chunk command
+E: No values in fix ave/chunk/ac command
 
 Self-explanatory.
 
-E: Cannot open fix ave/chunk file %s
+E: Cannot open fix ave/chunk/ac file %s
 
 The specified file cannot be opened.  Check that the path and name are
 correct.
@@ -113,59 +113,59 @@ E: Bias compute does not calculate a velocity bias
 
 The specified compute must compute a bias for temperature.
 
-E: Compute ID for fix ave/chunk does not exist
+E: Compute ID for fix ave/chunk/ac does not exist
 
 Self-explanatory.
 
-E: Fix ave/chunk compute does not calculate per-atom values
+E: Fix ave/chunk/ac compute does not calculate per-atom values
 
 Self-explanatory.
 
-E: Fix ave/chunk compute does not calculate a per-atom vector
+E: Fix ave/chunk/ac compute does not calculate a per-atom vector
 
 Self-explanatory.
 
-E: Fix ave/chunk compute does not calculate a per-atom array
+E: Fix ave/chunk/ac compute does not calculate a per-atom array
 
 Self-explanatory.
 
-E: Fix ave/chunk compute vector is accessed out-of-range
+E: Fix ave/chunk/ac compute vector is accessed out-of-range
 
 Self-explanatory.
 
-E: Fix ID for fix ave/chunk does not exist
+E: Fix ID for fix ave/chunk/ac does not exist
 
 Self-explanatory.
 
-E: Fix ave/chunk fix does not calculate per-atom values
+E: Fix ave/chunk/ac fix does not calculate per-atom values
 
 Self-explanatory.
 
-E: Fix ave/chunk fix does not calculate a per-atom vector
+E: Fix ave/chunk/ac fix does not calculate a per-atom vector
 
 Self-explanatory.
 
-E: Fix ave/chunk fix does not calculate a per-atom array
+E: Fix ave/chunk/ac fix does not calculate a per-atom array
 
 Self-explanatory.
 
-E: Fix ave/chunk fix vector is accessed out-of-range
+E: Fix ave/chunk/ac fix vector is accessed out-of-range
 
 Self-explanatory.
 
-E: Variable name for fix ave/chunk does not exist
+E: Variable name for fix ave/chunk/ac does not exist
 
 Self-explanatory.
 
-E: Fix ave/chunk variable is not atom-style variable
+E: Fix ave/chunk/ac variable is not atom-style variable
 
 Self-explanatory.
 
-E: Chunk/atom compute does not exist for fix ave/chunk
+E: Chunk/atom compute does not exist for fix ave/chunk/ac
 
 Self-explanatory.
 
-E: Fix ave/chunk does not use chunk/atom compute
+E: Fix ave/chunk/ac does not use chunk/atom compute
 
 The specified compute is not for a compute chunk/atom command.
 
@@ -173,12 +173,12 @@ E: Error writing file header
 
 Something in the output to the file triggered an error.
 
-E: Fix for fix ave/chunk not computed at compatible time
+E: Fix for fix ave/chunk/ac not computed at compatible time
 
-Fixes generate their values on specific timesteps.  Fix ave/chunk is
+Fixes generate their values on specific timesteps.  Fix ave/chunk/ac is
 requesting a value on a non-allowed timestep.
 
-E: Invalid timestep reset for fix ave/chunk
+E: Invalid timestep reset for fix ave/chunk/ac
 
 Resetting the timestep has invalidated the sequence of timesteps this
 fix needs to process.
